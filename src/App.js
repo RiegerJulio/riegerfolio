19e367e2ade4dbddd6300a6fc964887bfdc4dbcf
RiegerJulio <riegerproducoes@gmail.com>
2022-01-12 15:07:00 -0300

    Pagina About

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <Projects />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>"Eu prefiro ser temido ou amado? Fácil, ambos. Quero que as pessoas tenham medo do quanto elas me amam  ." - Michael Scott.</h3>
+        <img src="https://ca.slack-edge.com/TMDDFEPFU-U02FHMLRYN4-f4157b50ecae-512" alt="img-gero"/>
+        <p>Julio Rieger</p>
+        <a href="#">teste</a>
+        <a href="#">teste</a>
+        <a href="#">teste</a>
+        <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui do Lorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos </p>
+      </div>
+    )
+  }
+}
