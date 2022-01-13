e6027b49e2add3964017a53fdd0cdb92d3e70e39
RiegerJulio <riegerproducoes@gmail.com>
2022-01-13 15:28:51 -0300

    trocando cards para carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 export default class Header extends Component {
   render() {
     return (
@@ -7,6 +8,7 @@ export default class Header extends Component {
         <h1>Olá, eu sou o Rieger!</h1>
         <p>Sou um ex músico e comediante em transição de carreira para Desenvolvimento Web. Sejam bem vindos ao meu Portfólio</p>
       </div>
+      
     )
   }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react'
-import Card from "./Card"
+import M from 'materialize-css';
 
 export default class Projects extends Component {
+  componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.slider');
+      var instances = M.Slider.init(elems, {});
+    });
+  }
+
   render() {
     return (
       <div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+         <div className="slider">
+    <ul className="slides">
+      <li>
+        <img src="https://ca.slack-edge.com/TMDDFEPFU-U02FHMLRYN4-f4157b50ecae-512" alt="teste" /> 
+        <div className="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://ca.slack-edge.com/TMDDFEPFU-U02FHMLRYN4-f4157b50ecae-512" alt="teste" /> 
+        <div className="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://ca.slack-edge.com/TMDDFEPFU-U02FHMLRYN4-f4157b50ecae-512" alt="teste" /> 
+        <div className="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://ca.slack-edge.com/TMDDFEPFU-U02FHMLRYN4-f4157b50ecae-512" alt="teste" /> 
+        <div className="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
         <button><a href="https://github.com/riegerjulio">Veja meu repositório Completo de Projetos</a></button>
         <h1>Kit de Habilidades</h1>
         <ul>
-          <li>HTML 5</li>
-          <li>CSS 3</li>
-          <li>Flexbox e Grid Layout</li>
-          <li>Layout Responsivo - Mobile First</li>
-          <li>Materialize CSS e Bootstrap</li>
-          <li>Javascript ES6</li>
-          <li>Fetch API</li>
-          <li>React</li>
-          <li>Jest e RTL(TDD)</li>
-          <li>Versionamento com Git</li>
+          <li>- HTML 5</li>
+          <li>- CSS 3</li>
+          <li>- Flexbox e Grid Layout</li>
+          <li>- Layout Responsivo - Mobile First</li>
+          <li>- Materialize CSS e Bootstrap</li>
+          <li>- Javascript ES6</li>
+          <li>- Fetch API</li>
+          <li>- React</li>
+          <li>- Jest e RTL(TDD)</li>
+          <li>- Versionamento com Git</li>
         </ul>
       </div>
     )
