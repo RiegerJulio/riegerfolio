602f566402e4a7de68c6710b55b353fc1c34ee8c
RiegerJulio <riegerproducoes@gmail.com>
2022-02-25 10:30:50 -0300

    Mudando IMG e ADD Texto Formatado

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import riegerIMG from "../images/icon_julio.png"
+import riegerIMG from "../images/Rieger_IMG.png"
 import {FaGithubAlt, FaLinkedinIn} from "react-icons/fa"
 import {FiMail} from "react-icons/fi"
 
@@ -18,11 +18,17 @@ export default class About extends Component {
               <a href="mailto:riegerproducoes@gmail.com"><FiMail /></a>
             </div>
           </div>
-        <p>Meu nome é Rieger, tenho 28 anos e sou do Rio de Janeiro - RJ.
-Desde novo, tenho paixão pela área artistica e comecei a carreira de músico aos 12 anos. Durante a vida adulta, acabei me apaixonando por multimídia e formei em na área de produção musical. Além disso, fiz comédia Stand-up por quase 2 anos.
-Mas você pode estar se perguntando agora: O porque você decidiu ser desenvolvedor web?
-Durante uma conversa com um amigo sobre os rumos da minha vida, fui apresentado ao mundo do desenvolvimento. No inicio eu dizia "isso nao é pra mim, eu pertenço a arte" mas logo vi que não passava de medo de conquistar novos horizontes. Medo de sair da zona de conforto. Desde então, resolvi mergulhar fundo na área de desenvolvimento e comecei um curso de  Desenvolvimento Full Stack na Trybe. Minha vida mudou completamente, agora eu percebo que programação também é arte. É dar vida aos sonhos, desejos e vontades de outras pessoas, é me expressar e ser criativo através de linhas de código. Hoje posso dizer que é isso que eu quero pro resto da minha vida. Gerar impacto através dos meus códigos.
-</p>
+          <p><strong>Meu nome é Rieger, tenho 28 anos e sou do Rio de Janeiro - RJ.</strong>
+            <br />
+            <br />
+          Desde novo, tenho paixão pela área artística e comecei a carreira de músico aos 12 anos. Durante a vida adulta, acabei me apaixonando por multimídia e formei em na área de produção musical. Além disso, fiz comédia Stand-up por quase 2 anos, que é uma das minhas maiores paixões
+            <br />
+            <br />
+          Mas você pode estar se perguntando agora: <strong>Por que você decidiu ser desenvolvedor web?</strong>
+            <br />
+            <br />
+          Durante uma conversa com um amigo sobre os rumos da minha vida, fui apresentado ao mundo do desenvolvimento. No início eu dizia "isso nao é pra mim, eu pertenço a arte" mas logo vi que não passava de medo de conquistar novos horizontes e de sair da zona de conforto. Desde então, resolvi mergulhar fundo na área de desenvolvimento e comecei um curso de  Desenvolvimento Full Stack na <strong>Trybe</strong>. Desde então, vida mudou completamente e agora eu percebo que <strong>programação também é arte.</strong> É dar vida aos sonhos, desejos e vontades de outras pessoas, é me expressar e ser criativo através de linhas de código. Hoje posso dizer que é isso que eu quero pro resto da minha vida. <strong>Gerar impacto através dos meus códigos.</strong>
+          </p>
         </div>
       </article>
     )
